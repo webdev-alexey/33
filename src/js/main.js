641abf2f284202b2e9099e898e1a41352d3f11ff
webdev-alexey <alexey.anasov@yandex.by>
2023-12-16 18:05:32 +0300

    адаптив + интерактивные элементы

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
     loop: true,
-    centeredSlides: true,
-    slidesPerView: 1.4,
-    spaceBetween: 30,
-    initialSlide: 1,
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1,
+        spaceBetween: 20,
+        centeredSlides: false,
+      },
+      450: {
+        slidesPerView: 1.4,
+        spaceBetween: 30,
+        centeredSlides: true,
+        initialSlide: 1,
+      },
     },
   });
 });
